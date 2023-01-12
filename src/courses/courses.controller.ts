65d9b1c3b8e0f7bdd4ce46049b64b642e26c34ca
myildizCH <meyildiz.de@gmail.com>
2023-01-12 15:50:26 +0100

    feat(courses): add getCourseById() for GET request to controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CoursesService } from './courses.service';
+import { LepayaCourseOutputDTO } from './dto/lepaya-course-output.dto';
 
-@Controller('courses')
-export class CoursesController {}
+@Controller('api/lepaya-courses')
+export class CoursesController {
+  constructor(private readonly coursesService: CoursesService) {}
+
+  @Get('/:id')
+  async getCourseById(@Param('id') id: string): Promise<LepayaCourseOutputDTO> {
+    return this.coursesService.getCourseById(id);
+  }
+}
