20673d5fd4e0646c50db8577bc900d48f5ec65f2
myildizCH <meyildiz.de@gmail.com>
2023-01-12 15:13:19 +0100

    feat(courses): add getCourseById() method
    getByCourseById() method generates the output response.

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { CustomHttpService } from 'src/http/http.service';
+import { HRCourseInputDTO } from './dto/hr-course-input.dto';
+import { HRLearner, HRLearnerInputDTO } from './dto/hr-learner-input.dto';
+import { HRTrainerInputDTO } from './dto/hr-trainer-input.dto';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  constructor(private readonly httpService: CustomHttpService) {}
+
+  async getCourseById(id: string): Promise<any> {
+    // Fetch course by id
+    const course: HRCourseInputDTO = await this.httpService.fetch(
+      'courses',
+      id,
+    );
+
+    // Fetch trainer by id
+    const trainer: HRTrainerInputDTO = await this.httpService.fetch(
+      'trainers',
+      course.trainerId,
+    );
+
+    // get all learners
+    const { learners }: HRLearnerInputDTO = await this.httpService.fetch(
+      'learners',
+    );
+
+    // filter the learners by id
+    const filteredLearnersList: Array<HRLearner> = learners.filter((learner) =>
+      course.learners.includes(learner.id),
+    );
+
+    return { course, trainer, filteredLearnersList };
+  }
+}
