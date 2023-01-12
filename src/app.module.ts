e7a3017d4a5116b516bbcbb8016715c628dee66f
myildizCH <meyildiz.de@gmail.com>
2023-01-12 14:32:21 +0100

    feat(app): update app module
    Move controller and provider features to course module and import the course module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CoursesModule],
 })
 export class AppModule {}
